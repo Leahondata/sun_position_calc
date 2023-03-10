e18d698c20aa173f37fb15044174d0b01416c94f
Leahondata <8v99c8j5@duck.com>
2023-03-10 16:21:22 +0100

    Refactored code and added docstrings.

--- api_get_requests.py
+++ api_get_requests.py
@@ -1,41 +1,94 @@
 import requests
 import unicodedata
 from api_key import api_key
+import sys
 
 
-# API requests for city, region, and IP info.
-api1 = 'http://ipinfo.io/json'
-response = requests.get(api1)
-city = response.json()['city']
-region = response.json()['region']
-ip = response.json()["ip"]
+def detect_location():
+    '''
+    Detects the location details of the user by making API requests utilizing their IP address in.
+    If the request fails, the user is notified and the program exits.
+
+    Parameters:
+    None.
+
+    Returns:
+    dict: A dictionary containing the following keys:
+            - 'City': The city name of the user's ISP location.
+            - 'Region': The region name of the user's ISP location.
+            - 'IP': The IP address of the user's ISP location.
+
+    Raises:
+    requests.exceptions.HTTPError: If the request fails due to an invalid response code, it will notify the user and exits the program.
+    '''
+    loc_details = {}
+    api_url = 'http://ipinfo.io/json'
+    response = requests.get(api_url)
+    check_response_code(response.status_code, api_url)
+    loc_details['City'] = response.json()['city']
+    loc_details['Region'] = response.json()['region']
+    loc_details['IP'] = response.json()["ip"]
+    return loc_details
 
 
-# Second API request for country name, date, and time info.
-response4 = requests.get(
-    f"https://api.ipgeolocation.io/ipgeo?apiKey={api_key}&ip={ip}")
-country_name = response4.json()['country_name']
-date_time = response4.json()['time_zone']['current_time']
+def get_additional_loc_data(loc_details):
+    '''(dict) -> dict
+
+    Retrieves additional location data by making API requests based on the location details in the dictionary, and adds it to the dictionary.
+    If the request fails, the user is notified and the program exits.
+
+    Parameters:
+    loc_details (dict): A dictionary containing the location details, including the IP address.
+
+    Returns:
+    dict: The updated dictionary with the additional keys:
+            - 'Country_Name': The country name of the user's ISP location.
+            - 'Date_Time': The date and time of the user's ISP location.
+
+    Raises:
+    requests.exceptions.HTTPError: If the request fails due to an invalid response code, it will notify the user and exits the program.
+    '''
+    api_url = f'https://api.ipgeolocation.io/ipgeo?apiKey={api_key}&ip={loc_details["IP"]}'
+    response = requests.get(api_url)
+    check_response_code(response.status_code, api_url)
+    loc_details['Country_Name'] = response.json()['country_name']
+    loc_details['Date_Time'] = response.json()['time_zone']['current_time']
+    return loc_details
 
 
-# Check if input contains and ASCII characters.
-def check_isascii(check_input):
-    if check_input.isascii():
-        updated_input = check_input
-        # print(updated_input, 'is already correct format')
-    else:
-        # print(check_input, 'is not in the correct format')
-        # normalize string input to NFKD form
-        normalize = unicodedata.normalize('NFKD', check_input)
-        # translate NFKD form to ASCII equivalents
-        updated_input = normalize.encode('ascii', 'ignore').decode('ascii')
-    return updated_input
+def convert_to_ascii(loc_str):
+    '''(str) -> str
+
+    Converts a string to ASCII equivalents if the sting is not already in ASCII format.
+
+    Parameters:
+    loc_str (str): The input string to be converted to ASCII.
+
+    Returns:
+    str: The converted string in ASCII format.
+    '''
+    if not loc_str.isascii():
+        normalized_data = unicodedata.normalize('NFKD', loc_str)
+        loc_str = normalized_data.encode('ascii', 'ignore').decode('ascii')
+    return loc_str
 
 
-# Dictionary to hold the accumulated data.
-loc_details = {}
-loc_details['City'] = check_isascii(city)
-loc_details['Region'] = check_isascii(region)
-loc_details['Country_Name'] = check_isascii(country_name)
-loc_details['Date_Time'] = date_time
-loc_details['IP'] = ip
+def check_response_code(response_code, url):
+    '''(int, str) -> None
+
+    Checks the response of an HTTP request.
+        If the response is not 200, it notifies the user and exits the program.
+        If the response is 200, the fuction does nothing.
+
+    Parameters:
+    response_code (int): The response code of an HTTP request.
+    url (str): The URL of the HTTP request.
+
+    Returns:
+    None.
+    '''
+    if response_code != 200:
+        print('\n----------------------------------------------------------------')
+        print(f'{url} is currently down.\nPlease try again in a few minutes.')
+        print('----------------------------------------------------------------\n')
+        sys.exit()
--- sun_times_app.py
+++ sun_times_app.py
@@ -1,54 +1,120 @@
-from api_get_requests import loc_details
 from bs4 import BeautifulSoup
+import api_get_requests as agr
 import pandas as pd
 import requests
 import sys
 
-# Detect location of user using their IP address.
-print('\n--------------Your location--------------')
-print(f"\n{loc_details['City']},")
-print(f"{loc_details['Region']},")
-print(f"{loc_details['Country_Name']}.")
+
+def detect_location():
+    '''
+    Detects the location of the user and returns a dictionary of location data.
+
+    Parameters:
+    None.
+
+    Returns:
+    dict: A dictionary containing the following keys:
+            - 'City': The city name of the user's ISP location.
+            - 'Region': The region name of the user's ISP location.
+            - 'IP': The IP address of the user's ISP location.
+            - 'Country_Name': The country name of the user's ISP location.
+            - 'Date_Time': The date and time of the user's ISP location.
+
+    Raises:
+    requests.exceptions.HTTPError: If the request fails due to an invalid response code, it will notify the user and exits the program.
+    '''
+    temp_dict = agr.detect_location()
+    temp_dict = agr.get_additional_loc_data(temp_dict)
+    temp_dict['City'] = agr.convert_to_ascii(temp_dict["City"])
+    return temp_dict
 
 
-# Create and return url with imported data from location_detector.
-url = (f"https://sunrise-sunset.org/search?location={loc_details['City']}")
+def construct_scraper_url(city_str):
+    '''(str) -> str
+
+    Constructs and returns a URL for webscraping application that retrieves sun data based on the detected location.
+
+    Parameters:
+    city_str (str): The name of the city to be passed into the creation of the URL.
+
+    Returns:
+    str: A string representing the created URL to be used in webscraping application.
+    '''
+    url = (f"https://sunrise-sunset.org/search?location={city_str}")
+    return url
 
 
-# Make the request using the generated IP address.
-response = requests.get(url)
-status = response.status_code
+def request_data(url):
+    '''(str) -> bs4.BeautifulSoup
+
+    Sends an HTTP request to the specified URL and returns the response as a Beautiful Soup object.
+
+    Parameters:
+    url (str): A string representing the created URL to be used in webscraping application.
+
+    Returns:
+    bs4.BeautifulSoup: A Beautiful Soup object that contains the response content from the URL.
+
+    Raises:
+    requests.exceptions.HTTPError: If the request fails due to an invalid response code, it will notify the user and exits the program.
+    '''
+    print('\n')
+    print('Fetching location data...')
+    response = requests.get(url)
+    agr.check_response_code(response.status_code, url)
+    soup_object = BeautifulSoup(response.content, 'html.parser')
+    return soup_object
 
 
-# Handle error codes.
-if status != 200:
-    print('\n----------------------------------------------------------------')
-    print('The server is currently down. Please try again in a few minutes.')
-    print('----------------------------------------------------------------\n')
-    sys.exit()
+def extract_table(page_content):
+    '''(bs4.BeautifulSoup) -> bs4.element.Tag
+
+    Extracts the HTML table containin the data to be scraped from the Beautiful Soup object and returns the table's data.
+
+    Parameters:
+    page_content (bs4.BeautifulSoup): A Beautiful Soup object that contains the response content from the URL.
+
+    Returns:
+    bs4.element.Tag: A Beautiful Soup Tag object representing the selected table.
+    '''
+    table = page_content.find('table', {'id': 'month'})
+    return table
 
 
-# Convert response to beautiful soup object.
-soup_object = BeautifulSoup(response.content, 'html.parser')
+def parse_table_data(table_data):
+    '''(bs4.element.Tag) -> tup
+
+    Parses the contents of the HTML table that is represented by a Beautiful Soup Tag object, extracting the table data and column headers.
+
+    Parameters:
+    table_data (bs4.element.Tag): A Beautiful Soup Tag object representing the selected table to be parsed.
+
+    Returns:
+    tuple: [lst[lst[str]]], lst[str]: A tuple containing two lists;
+            - The table data as a list of rows where each row is represented as a list of stings.
+            - The column headers as a list of stings.
+    '''
+    table_list = [[cell.text.strip() for cell in row.find_all('td')]
+                  for row in table_data.find_all('tr')]
+    headers_list = [header.text.strip()
+                    for header in table_data.find_all('th')]
+    return table_list, headers_list
 
 
-# Find the table to scrape data from.
-table = soup_object.find('table', {'id': 'month'})
-
-
-# Extract the table data and save it in a variable.
-table_data = [[cell.text.strip() for cell in row.find_all('td')]
-              for row in table.find_all('tr')]
-
-
-# Extract column headers.
-headers_data = []
-for header in table.find_all('th'):
-    headers_data.append(header.text.strip())
-
-
-# Separate header titles from days of the month.
 def separate_headers(headers_data):
+    '''(lst[str]) -> tup
+
+    Separate header titles from subheaders and days of the month.
+
+    Parameters:
+    headers_data (lst of str): The column headers as a list of strings to separate.
+
+    Returns:
+    tuple: lst[str], lst[str], lst[str]: A tuple containing three lists:
+            - The separated header titles.
+            - The separated subheaders.
+            - The separated days of the month.
+    '''
     header_titles = headers_data[:8]
     header_titles.insert(8, '')
     header_titles.extend([headers_data[8], ''])
@@ -57,38 +123,77 @@ def separate_headers(headers_data):
     return header_titles, sub_headers, month_days
 
 
-header_titles, sub_headers, month_days = separate_headers(headers_data)
+def construct_df(table_list, header_titles, month_days):
+    '''(lst[str], lst[str], lst[str]) -> DataFrame
+
+    Organize the data and then create the data frame using the data in the organized lists.
+
+    Parameters:
+        table_list (lst[str]): The list of data to construct the data frame with.
+        header_titles (lst[str]): The header titles to use for the data frame.
+        month_days (lst[str]): The days of the month to use in the data frame.
+
+    Returns:
+    DataFrame: A pandas DataFrame containing the organized data.
+    '''
+    del table_list[:2]
+    data = [[month_days[i]] + table_list[i] for i in range(len(month_days))]
+    headers = pd.MultiIndex.from_arrays([header_titles, sub_headers])
+    df = pd.DataFrame(data, columns=headers)
+    return df
 
 
-# Delete the first two empty lists within table_data.
-del table_data[:2]
+def display_output(df, loc_dict, gen_csv=False):
+    '''(df, dict, bool) -> None
+
+    Displays the location details, fetches the data for today's date based on those location details, and prints it to the console.
+    Optionally generates two CSV files.
+        If gen_csv is True, the function generates two CSV files:
+            - One with the monthly data by location.
+            - Another with the data for today's date only.
+
+    Parameters:
+        df (DataFrame): A pandas Dataframe containing the data to display.
+        loc_dict (dict): A dictionary containing the location details, including the city, region, country name and date/time.
+        gen_csv (bool): A flag indicating whether to generate a CSV file or not. Default is False.
+
+    Returns:
+    None.
+    '''
+    print('--------------Your location--------------')
+    print(f"\n{loc_dict['City']},")
+    print(f"{loc_dict['Region']},")
+    print(f"{loc_dict['Country_Name']}.")
+    print('-----------------------------------------')
+    print('\n')
+
+    print('Fetching data based on your location...')
+
+    today = int(loc_dict['Date_Time'].split(' ')[0].split('-')[2]) - 1
+
+    print("---------------Today's data--------------")
+    row = df.loc[today].to_frame()
+    empty_header = ['']
+    row.columns = empty_header
+    print(row)
+    print('-----------------------------------------', '\n')
+    if gen_csv:
+        df.to_csv("monthly_data_by_loc.csv", index=True)
+        row.to_csv('todays_data_by_loc.csv', index=True, header=None)
 
 
-# Combine month_days and table_data into one list to pass to dataframe.
-data = [[month_days[i]] + table_data[i] for i in range(len(month_days))]
+location_data = detect_location()
 
+webpage_raw_data = construct_scraper_url(location_data['City'])
 
-# Combine header_titles and sub_headers into one list to pass to dataframe.
-headers = pd.MultiIndex.from_arrays([header_titles, sub_headers])
+webpage_soup_object = request_data(webpage_raw_data)
 
+table_data = extract_table(webpage_soup_object)
 
-# Create dataframe to organize and store data in.
-df = pd.DataFrame(data, columns=headers)
+table_list, headers_list = (parse_table_data(table_data))
 
+header_titles, sub_headers, month_days = separate_headers(headers_list)
 
-# Create CSV and save data for month in CSV file.
-df.to_csv("sun_data_by_loc.csv", index=False)
+df = construct_df(table_list, header_titles, month_days)
 
-
-# Begin user output message.
-print("---------------Today's data--------------")
-
-
-# Get today's date from loc_details['Date_Time'].
-today_single = int(loc_details['Date_Time'].split(' ')[0].split('-')[2]) - 1
-
-
-# Print the specified date with clean formatting for visibility.
-row = df.loc[today_single].to_frame().rename(columns={7: ''})
-print(row)
-print('-----------------------------------------', '\n')
+display_output(df, location_data, gen_csv=True)
